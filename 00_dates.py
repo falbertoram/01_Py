4ca2ce66bd4761a2c802fe5a5a8022ff4cc049a5
Alberto Ramirez <alberto.ramirez@gmail.com>
2023-02-14 16:26:10 -0300

    init date 2

--- 00_dates.py
+++ 00_dates.py
@@ -1,3 +1,43 @@
 #####################
 # Dates
 #####################
+
+from datetime import datetime
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second) 
+    print(date.timestamp())
+
+now = datetime.now()
+
+#print(now)
+
+print_date(now)
+
+#print(now.year)
+#print(now.month)
+#print(now.day)
+#print(now.hour)
+#print(now.minute)
+#print(now.second)
+
+#timestamp = now.timestamp() # Since 1970
+
+#print(timestamp)
+
+year_2023 = datetime(2023, 1, 1)
+print_date(year_2023)
+
+
+
+
+
+
+
+
+
